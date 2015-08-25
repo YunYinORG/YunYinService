7d13e1e4b92c6f043185e838a8be9eb3c5ecb5c4
New Future <new_future@msn.cn>
2015-08-25 23:08:34 +0800

    file controller 完成

--- app/controllers/File.php
+++ app/controllers/File.php
@@ -2,10 +2,13 @@
 /**
  * 文件管理
  */
+use Service\File;
+
 class FileController extends Rest
 {
 	/**
 	 * 文件列表
+	 * GET /file/
 	 * @method GET_index
 	 * @author NewFuture
 	 */
@@ -18,26 +21,79 @@ class FileController extends Rest
 
 	/**
 	 * 上传文件
+	 * POST /file/
 	 * @method POST_index
+	 * @param key 获取token时返回的key
 	 */
 	public function POST_indexAction()
 	{
-		//TO DO
-		//上传
+		$userid             = $this->auth();
+		$response['status'] = 0;
+		if (!Input::post('key', $key, 'trim'))
+		{
+			$response['info'] = '未收到数据';
+		}
+		elseif (!$name = Cookie::get($key))
+		{
+			$response['info'] = '文件不存在';
+		}
+		else
+		{
+			Cookie::del($key);
+			//文件名由 temp_xxxx,重命名为 file_xxxx,
+			$newfile        = strtr($key, 'temp', 'file') . '_' . $_SERVER['REQUEST_TIME'];
+			$file['name']   = $name;
+			$file['url']    = $newfile;
+			$file['use_id'] = $userid;
+
+			if (!File::set($key, $newfile)) //修改文件名
+			{
+				$response['info'] = '文件校验失败';
+			}
+			elseif (!$fid = FileModel::add($file));
+			{
+				$response['status'] = 1;
+				$response['info']   = '保存成功';
+				$response['id']     = $fid;
+			}
+		}
+		$this->response = $response;
 	}
 
 	/**
 	 * 获取上传token
+	 * POST /file/token
 	 * @method POST_token
+	 * @param name 文件名
 	 */
 	public function POST_tokenAction()
 	{
-		//TO DO
-		//上传
+		$userid = $this->auth();
+		if (Input::put('name', $name, 'tirm'))
+		{
+			$key   = uniqid('temp_' . $use_id . '.');
+			$token = File::getToken('temp_' . $key);
+			if ($token)
+			{
+				Cookie::set($key, $name);
+				$response['token'] = $files;
+				$response['key']   = $key;
+				$this->response(1, $response);
+			}
+			else
+			{
+				$this->response(0, 'token获取失败');
+			}
+		}
+		else
+		{
+			$this->response(0, '文件名无效');
+		}
 	}
 
 	/**
 	 * 文件列表
+	 * GET /file/1
 	 * @method GET_info
 	 * @author NewFuture
 	 */
@@ -56,8 +112,10 @@ class FileController extends Rest
 
 	/**
 	 * 文件信息修改
+	 * PUT /file/1
 	 * @method PUT_info
 	 * @author NewFuture
+	 * @param name 文件名
 	 */
 	public function PUT_infoAction($id = 0)
 	{
