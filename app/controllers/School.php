7a3179016d2218e44717e350a0b708a848d53751
New Future <new_future@msn.cn>
2015-09-08 22:07:31 +0800

    School Controller

--- app/controllers/School.php
+++ app/controllers/School.php
@@ -52,16 +52,48 @@ class SchoolController extends Rest
 	 * @param  integer        $id [description]
 	 * @author NewFuture
 	 */
-	public function GET_codeAction($id = 1)
+	public function GET_codeAction($id = 0)
 	{
-		if ($img = Verify\TJU::getCode())
+		if ($img = School::code($id))
 		{
 			$this->response(1, 'data:image/png;base64,' . base64_encode($img));
 		}
-		else
+		elseif ($img === false)
 		{
 			$this->response(0, '无需验证码');
+		}
+		else
+		{
+			Log::write('验证码获取失败' . $id, 'ERROR');
+			$this->response(-3, '验证码获取失败');
+		}
+	}
 
+	public function POST_numberAction()
+	{
+		if (Input::post('number', $number, 'card'))
+		{
+			Input::post('white', $white);
+			Input::post('black', $black);
+			if ($schools = School::guess($number, $black, $white))
+			{
+				if ($reg = UserModel::where('number', $number)->select('sch_id'))
+				{
+					foreach ($reg as $user)
+					{
+						$schools[$user['sch_id']] = 0;
+					}
+				}
+				$this->response(1, $schools);
+			}
+			else
+			{
+				$this->response(0, '无相关学校,请检查学号是否正确');
+			}
+		}
+		else
+		{
+			$this->response(0, '学号格式有误');
 		}
 	}
 }
