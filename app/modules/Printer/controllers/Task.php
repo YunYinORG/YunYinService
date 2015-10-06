62c2bf6a243f3adfa772e87e34bac8bbcd644689
New Future <new_future@msn.cn>
2015-10-07 01:34:37 +0800

    task更新

--- app/modules/Printer/controllers/Task.php
+++ app/modules/Printer/controllers/Task.php
@@ -10,37 +10,17 @@ class TaskController extends PrinterRest
 	 * @return [type]      [description]
 	 * @author NewFuture
 	 */
-	public function loginAction()
+	public function GET_listAction()
 	{
-		$response['status'] = 0;
-		if (!Input::post('account', $account, Config::get('regex.account')))
-		{
-			$response['info'] = '账号格式错误';
-		}
-		elseif (!Input::post('password', $account, 'isMd5'))
-		{
-			$response['info'] = '密码未加密处理';
-		}
-		elseif (!Safe::checkTry('printer_auth_' . $account))
-		{
-			$response['info'] = '尝试次数过多账号临时封禁,稍后重试或者联系我们';
-		}
-		elseif (!$Printer = PrinterModel::field('id,password,status,name')->find())
-		{
-			$response['info'] = '账号错误';
-		}
-		elseif (Encrypt::encryptPwd($password, $account) != $Printer['password'])
-		{
-			$response['info'] = '密码错误';
-		}
-		else
-		{
-			Safe::del('printer_auth_' . $account);
-			unset($Printer['password']);
-			$response['status'] = 1;
-			$response['info']   = $Printer;
-		}
-		$this->response = $response;
+		$userid = $this->auth();
+		Input::get('page', $page, 'int', 1);
+		$tasks = TaskModel::where('use_id', '=', $userid)->belongs('user')->page($page)->select();
+		$this->response(1, $tasks);
+	}
+
+	public function PUT_infoAction($id)
+	{
+		$userid = $this->auth();
 	}
 }
 ?>
